96146158a504ee8a4f0e3eaeb5fe18f341f98b8a
Dylan Waits <dylan@waits.io>
2015-06-07 03:18:41 +0000

    accept and return JSON

--- url.rb
+++ url.rb
@@ -10,11 +10,13 @@ config = JSON.parse json
 
 post '/' do
 	key = SecureRandom::urlsafe_base64(4)
-	value = request.body.read
+	json = JSON.parse request.body.read
+	value = json['original']
+	expires = if json.has_key? 'expires' then json['expires'] else 2592000 end
 	if value =~ /^https?:\/\/([a-z0-9-]+\.)+[a-z]{2,}(\/([a-zA-Z0-9-_]+(\.[a-z]+)?)?)*\??([a-z0-9_]+(=[a-z0-9_]*)?&?)*$/i
 	  while redis.get("url:#{key}") do key = SecureRandom::urlsafe_base64(4) end
-	  redis.set("url:#{key}", value, :ex => 86400)
-	  "#{request.scheme}://#{request.host_with_port}/#{key}"
+	  redis.set("url:#{key}", value, :ex => expires)
+	  {:id => key, :url => "#{request.scheme}://#{request.host_with_port}/#{key}"}.to_json
 	else
 		puts "Invalid URL: \"#{value}\""
 		halt 422, 'Invalid URL'
