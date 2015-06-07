61394268bf44fa5237f9905ebdbb2ddce8821875
Dylan Waits <dylan@waits.io>
2015-06-07 03:25:49 +0000

    cap expires at 30 days

--- url.rb
+++ url.rb
@@ -12,11 +12,11 @@ post '/' do
 	key = SecureRandom::urlsafe_base64(4)
 	json = JSON.parse request.body.read
 	value = json['original']
-	expires = if json.has_key? 'expires' then json['expires'] else 2592000 end
+	expires = if json.has_key? 'expires' and json['expires'] < 2592000 then json['expires'] else 2592000 end
 	if value =~ /^https?:\/\/([a-z0-9-]+\.)+[a-z]{2,}(\/([a-zA-Z0-9-_]+(\.[a-z]+)?)?)*\??([a-z0-9_]+(=[a-z0-9_]*)?&?)*$/i
 	  while redis.get("url:#{key}") do key = SecureRandom::urlsafe_base64(4) end
 	  redis.set("url:#{key}", value, :ex => expires)
-	  {:id => key, :url => "#{request.scheme}://#{request.host_with_port}/#{key}"}.to_json
+	  {:id => key, :url => "#{request.scheme}://#{request.host_with_port}/#{key}", :expires => expires}.to_json
 	else
 		puts "Invalid URL: \"#{value}\""
 		halt 422, 'Invalid URL'
