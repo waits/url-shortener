b309c48d39d08e9f3a0fc064ef346174ca85dc1d
Dylan Waits <dylan@waits.io>
2015-06-07 22:37:20 +0000

    returns correct HTTP status codes and headers

--- url.rb
+++ url.rb
@@ -11,15 +11,16 @@ config = JSON.parse json
 post '/' do
 	key = SecureRandom::urlsafe_base64(4)
 	json = JSON.parse request.body.read
-	value = json['original']
+	value = json['url']
 	expires = if json.has_key? 'expires' and json['expires'] < 2592000 then json['expires'] else 2592000 end
 	if value =~ /^https?:\/\/([a-z0-9-]+\.)+[a-z]{2,}(\/([a-zA-Z0-9-_]+(\.[a-z]+)?)?)*\??([a-z0-9_]+(=[a-z0-9_]*)?&?)*$/i
 	  while redis.get("url:#{key}") do key = SecureRandom::urlsafe_base64(4) end
 	  redis.set("url:#{key}", value, :ex => expires)
-	  {:id => key, :url => "#{request.scheme}://#{request.host_with_port}/#{key}", :expires => expires}.to_json
+	  short_url = "#{request.scheme}://#{request.host_with_port}/#{key}"
+	  [200, {'Content-Type' => 'application/json', 'Location' => short_url}, {:id => key, :expires => expires}.to_json]
 	else
 		puts "Invalid URL: \"#{value}\""
-		halt 422, 'Invalid URL'
+		halt 422, {'Content-Type' => 'text/plain'}, 'Invalid URL'
   end
 end
 
@@ -29,7 +30,7 @@ get '/:key' do
   if value
     redirect value, 302
   else
-    "Sorry, that URL doesn't exist or has expired."
+    [404, {'Content-Type' => 'text/plain'}, "Sorry, that URL doesn't exist or has expired."]
   end
 end
 
