49838fbf19bada043f4b62f11f912626cf2ee2a7
Dylan Waits <dylan@waits.io>
2015-07-03 23:46:06 +0000

    removed i from URL regex

--- url.rb
+++ url.rb
@@ -13,7 +13,7 @@ post '/' do
 	json = JSON.parse request.body.read
 	value = json['url']
 	expires = if json.has_key? 'expires' and json['expires'] < 2592000 then json['expires'] else 2592000 end
-	if value =~ %r@https?://(-\.)?([^\s/?\.#-]+\.?)+(/[^\s]*)?$@i
+	if value =~ %r@^https?://(-\.)?([^\s/?\.#-]+\.?)+(/[^\s]*)?$@
 	  while redis.get("url:#{key}") do key = SecureRandom::urlsafe_base64(5) end
 	  redis.set("url:#{key}", value, :ex => expires)
 	  short_url = "#{request.scheme}://#{request.host_with_port}/#{key}"
@@ -26,9 +26,8 @@ end
 
 get '/:key' do
   key = 'url:' + params['key']
-  value = redis.get(key)
-  if value
-    redirect value, 302
+  if value = redis.get(key)
+    redirect value, 301
   else
     [404, {'Content-Type' => 'text/plain'}, "Sorry, that URL doesn't exist or has expired."]
   end
